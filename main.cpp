05b892e4c1baa439d46fdc59448ca4e3eb6cb870
加贺加贺岬 <mengsiming77@gmail.com>
2021-09-09 18:36:36 +0800

    更改main.cpp文件选择逻辑

--- main.cpp
+++ main.cpp
@@ -8,33 +8,33 @@ int main() {
     Book addressBook;
     while (true) {
         addressBook.showmenu();
-        int choice;
+        char choice;
         cin >> choice;
 
         switch (choice) {
-            case 1: {
+            case '1': {
                 addressBook.show();
             } break;
-            case 2: {
+            case '2': {
                 addressBook.add();
             } break;
-            case 3: {
+            case '3': {
                 addressBook.search();
             } break;
-            case 4: {
+            case '4': {
                 addressBook.mod();
             } break;
-            case 5: {
+            case '5': {
                 addressBook.del();
             } break;
-            case 6: {
+            case '6': {
                 addressBook.sort();
             } break;
-            case 0: {
+            case '0': {
                 exit(0);
             } break;
             default: {
-                continue;
+                cin.sync();
             } break;
         }
 
